a500288c2dda199e838bb279770e7fbed9a84afe
Ishmael <151128995+Ishmael-quant@users.noreply.github.com>
2026-01-01 11:04:41 +0300

    Update login.php

--- login.php
+++ login.php
@@ -22,6 +22,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <title>Admin Login</title>
 <style>
     body {
@@ -72,4 +74,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </div>
 
 </body>
+
 </html>
